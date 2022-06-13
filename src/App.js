8f1510cbbd88bbeffd9f08567fb5cf69d3784880
AplinXy9plin <aplinxy9plin@gmail.com>
2022-06-13 20:17:01 +0700

    zora api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Web3 from "web3";
-import { Container, Content, FlexboxGrid, Button } from "rsuite";
+import { Container, Content, FlexboxGrid, Button, Footer, Stack, Navbar } from "rsuite";
+import { ZDK, ZDKNetwork, ZDKChain } from "@zoralabs/zdk";
+import { MediaObject } from "@zoralabs/nft-components";
 
 // components
 import ModalSuccess from "./components/ModalSuccess";
@@ -10,14 +12,49 @@ import ABI from "./ABI.json";
 
 const URL = "http://www.thischimpdoesnotexist.com:8080";
 
+const networkInfo = {
+  network: ZDKNetwork.Ethereum,
+  chain: ZDKChain.Mainnet,
+}
+
+const API_ENDPOINT = "https://api.zora.co/graphql";
+const args = { 
+            endPoint:API_ENDPOINT, 
+            networks:[networkInfo], 
+            // apiKey: process.env.API_KEY 
+          } 
+
+const zdk = new ZDK(args) // All arguments are optional
+
 const App = () => {
   const [uuid, setUuid] = React.useState("");
   const [txHash, setTxHash] = React.useState("");
   const [isOpen, setIsOpen] = React.useState(false);
   const [isLoading, setIsLoading] = React.useState(false);
+  const [tokensUrls, setTokensUrls] = React.useState([])
 
   React.useEffect(() => {
     init();
+    zdk.tokens({
+      where: {
+        "collectionAddresses": "0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d"
+      },
+      "includeFullDetails": true,
+      pagination: {
+        limit: 6
+      }
+    }).then((tokens) => {
+      if(tokens.tokens.nodes){
+        let result = []
+        for (let i = 0; i < tokens.tokens.nodes.length; i++) {
+          const item = tokens.tokens.nodes[i]
+          if(item.token.image){
+            result.push(item.token.image.url)
+          }
+        }
+        setTokensUrls(result)
+      }
+    })
   }, []);
 
   const init = async () => {
@@ -53,27 +90,31 @@ const App = () => {
     setIsLoading(true);
     if (window.ethereum) {
       const web3 = new Web3(window.ethereum);
-      const PUBLIC_KEY = await connectWallet();
-      if (PUBLIC_KEY) {
-        const ipfsData = await uploadToIPFS();
-        if (ipfsData) {
-          if (ipfsData.IpfsHash) {
-            const contractAddress =
-              "0xB4798e07966Ce521Eac0E969a93DB3Dce60e8a1E";
-            const nftContract = new web3.eth.Contract(ABI, contractAddress);
-            await mintNFT(
-              `https://gateway.pinata.cloud/ipfs/${ipfsData.IpfsHash}`,
-              web3,
-              PUBLIC_KEY,
-              contractAddress,
-              nftContract
-            );
+      if(await web3.eth.getChainId() !== 4){
+        const PUBLIC_KEY = await connectWallet();
+        if (PUBLIC_KEY) {
+          const ipfsData = await uploadToIPFS();
+          if (ipfsData) {
+            if (ipfsData.IpfsHash) {
+              const contractAddress =
+                "0xB4798e07966Ce521Eac0E969a93DB3Dce60e8a1E";
+              const nftContract = new web3.eth.Contract(ABI, contractAddress);
+              await mintNFT(
+                `https://gateway.pinata.cloud/ipfs/${ipfsData.IpfsHash}`,
+                web3,
+                PUBLIC_KEY,
+                contractAddress,
+                nftContract
+              );
+            } else {
+              alert("Server uploading error");
+            }
           } else {
             alert("Server uploading error");
           }
-        } else {
-          alert("Server uploading error");
         }
+      }else{
+        alert("Please select Rinkeby network")
       }
     } else {
       alert("Please install MetaMask");
@@ -106,7 +147,10 @@ const App = () => {
 
   return (
     <Container>
-      <Content>
+      <Navbar>
+        <p>This version is using Rinkeby network now.</p>
+      </Navbar>
+      <Content style={{ marginTop: 16 }}>
         <FlexboxGrid justify="center">
           <FlexboxGrid.Item>
             {uuid && <img alt="nftImage" src={`${URL}/image/${uuid}`} />}
@@ -126,6 +170,21 @@ const App = () => {
           </FlexboxGrid.Item>
         </FlexboxGrid>
       </Content>
+      <Footer style={{ marginTop: 16, backgroundColor: "grey", padding: 3 }}>
+        <h1 style={{ color: "white" }}>Recent generated</h1>
+        <Stack spacing={4}>
+          {
+            tokensUrls && (
+              tokensUrls.map((item, index) =>
+                <MediaObject
+                  key={`recentNFT_${index}`}
+                  contentURI={item}
+                />
+              )
+            )
+          }
+        </Stack>
+      </Footer>
       <ModalSuccess
         isOpen={isOpen}
         setIsOpen={setIsOpen}
